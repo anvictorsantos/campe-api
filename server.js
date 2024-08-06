5aa8d23384301e36f84565831ccf7f9ca676417f
Antonio Pontes <anvictorsantos@gmail.com>
2024-08-06 07:56:58 +0100

    feat: custom and morgan middlewares

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+// Import necessary modules
 const express = require("express");
 const dotenv = require("dotenv");
-
-const bootcamps = require("./routes/bootcamps");
-
+const morgan = require("morgan");
 dotenv.config({ path: "./config/config.env" });
 
+// Import custom middleware and routes
+const logger = require("./middleware/logger");
+const bootcamps = require("./routes/bootcamps");
+
+// Create an Express application
 const app = express();
 
+// Use dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Use the custom logger middleware
+app.use(logger);
+
+// Bootcamps routes
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Start the server
 const PORT = process.env.PORT || 3000;
-
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
